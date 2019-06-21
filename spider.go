b0be0b54b684618e2e6ba2544bacaa068537e856
manudevelopia <manu@developia.info>
2019-06-21 06:47:38 +0200

    feat: retrieve compose name and vla values

--- spider.go
+++ spider.go
@@ -6,58 +6,59 @@ import (
 )
 
 type Note struct {
-	Code  string
-	Title string
+	code  string
+	title string
 }
 
 type Compose struct {
-	Link  string
-	Name  string
-	VlaEd string
-	VlaEc string
-	Notes []Note
+	link  string
+	name  string
+	vlas  [4]string
+	notes []Note
 }
 
 func main() {
 
-	composes := make([]Compose, 0, 2000)
+	var composes []Compose
+	compose := Compose{}
+	pages := 1 //46
 
+	// Create collector
 	c := colly.NewCollector(
 		colly.AllowedDomains("bdlep.inssbt.es"),
 	)
 
-	c.OnHTML("a[href*=nombre]", func(e *colly.HTMLElement) {
-		link := e.Attr("href")
+	c.OnRequest(func(request *colly.Request) {
+		fmt.Println("Visiting: ", request.URL)
+	})
 
-		fmt.Printf("Link found: %q -> %s\n", e.Text, link)
+	// Get all compose links
+	c.OnHTML("table[class='contents'] a[href*=nombre]", func(a *colly.HTMLElement) {
+		fmt.Println(a.Text)
 
-		compose := Compose{
-			Link: link,
-			Name: e.Text,
-		}
+		compose.link = a.Request.AbsoluteURL(a.Attr("href"))
+		compose.name = a.Text
+
+		_ = c.Visit(compose.link)
+	})
+
+	// get the 4 environmental values VLA-ED and VLA-EC
+	c.OnHTML("table[class='valores'] tr:not([class='cabecera']) td", func(td *colly.HTMLElement) {
+		fmt.Printf("VL :: %s - %d\n", td.Text, td.Index)
+
+		compose.vlas[td.Index] = td.Text
+	})
+
+	// link has been scrapped
+	c.OnScraped(func(r *colly.Response) {
+		fmt.Println(" - Finished", r.Request.URL)
 
 		composes = append(composes, compose)
-
-		fmt.Printf("Compound : %s\n", compose)
-
-		c.OnHTML("td", func(td *colly.HTMLElement) {
-			fmt.Printf("Value found: %s\n", td.Text)
-		})
-
-		c.Visit(e.Request.AbsoluteURL(compose.Link))
 	})
 
-	c.OnRequest(func(r *colly.Request) {
-		fmt.Println("Visiting", r.URL.String())
-	})
-
-	c.OnScraped(func(r *colly.Response) {
-		fmt.Println("Finished", r.Request.URL)
-
-		fmt.Printf("Composes %s", composes)
-	})
-
-	// Start scraping on https://hackerspaces.org
-	c.Visit("http://bdlep.inssbt.es/LEP/vlaallpr.jsp?Bloque=1&submit=Listado+completo+Agentes+Qu%C3%ADmicos")
+	// Iterate on each page ::
+	for i := 1; i <= pages; i++ {
+		_ = c.Visit(fmt.Sprintf("http://bdlep.inssbt.es/LEP/vlaallpr.jsp?Bloque=%d", i))
+	}
 
 }
